e9d30e0ef259b5d4600810f6cc85f493de560761
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-10-28 22:02:34 -0500

    Implemented request protocol

---
+++ lab1/protocol.py
@@ -0,0 +1,38 @@
+import struct
+from util import *
+
+# struct {
+#   short header; = 0 0 00000101100100
+#   byte labN;   = 00000001
+#   byte version; = 00000111
+#   int clientCookie;
+#   int requestData;
+#   short checksum;
+#   short result;
+# }
+
+
+def package_request(requestData, clientCookie):
+    header = 356
+    labN = 1
+    version  = 7
+    checksum = 0
+    result = 0
+
+    tempPackage  = struct.pack('!HBBIIHH', header, labN, version, clientCookie, requestData, checksum, result)
+    checksum = calc_checksum(tempPackage)
+    return struct.pack('!HBBIIHH', header, labN, version, clientCookie, requestData, checksum, result)
+
+def print_raw(response):
+    for i in range(4):
+        shortLow = get_short_network(tempPackage, i*4)
+        shortHight = get_short_network(tempPackage, i*4 + 2)
+        lowString = "{0:b}".format(shortLow)
+        highString = "{0:b}".format(shortHight)
+        print(lowString,  " ", highString)
+
+def calc_checksum(tempPackage):
+    checksumTotal = 0
+    for i in range(8):
+        shortVal = get_short_network(tempPackage, i*2)
+        checksumTotal = ones_comp_add16(checksumTotal, shortVal)
--- lab1/util.py
+++ lab1/util.py
@@ -1,9 +1,3 @@
-# Courtesy of http://stackoverflow.com/a/29843639
-MOD = 1 << 16
-def ones_comp_add16(num1,num2):
-    result = num1 + num2
-    return result if result < MOD else (result+1) % MOD
-
 def get_byte(string, pos):
     return ord(string[pos])
 
@@ -16,3 +10,10 @@ def get_int_network(string, pos):
     sig3 = ord(string[pos + 2]) << 8
     sig4 = ord(string[pos + 3])
     return sig1 + sig2 + sig3 + sig4
+
+
+# Courtesy of http://stackoverflow.com/a/29843639
+MOD = 1 << 16
+def ones_comp_add16(num1,num2):
+    result = num1 + num2
+    return result if result < MOD else (result+1) % MOD
